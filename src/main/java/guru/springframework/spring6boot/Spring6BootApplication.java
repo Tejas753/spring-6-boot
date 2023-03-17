e27f5714da89566834b6e2db9549c86e0a9cf074
Tejas Sinapanahalli Gangadharaiah <tejas.sg753@gmail.com>
2023-03-17 04:00:29 -0500

    New Project

---
+++ src/main/java/guru/springframework/spring6boot/Spring6BootApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6BootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6BootApplication.class, args);
+	}
+
+}
